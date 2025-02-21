6ebd0bb1e4d81e89e796758355ed2aca67e25b47
Mike Matiunin <plugfox@gmail.com>
2025-02-22 02:51:55 +0400

    Save logs

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:collection';
 import 'dart:developer';
 
 import 'package:intl/intl.dart';
@@ -8,15 +9,20 @@ import 'package:vixen/vixen.dart';
 const String updateIdKey = 'update_id';
 
 void main(List<String> args) {
+  final logsBuffer = Queue<LogMessage>();
+  l.listen(logsBuffer.add, cancelOnError: false);
+
   final arguments = Arguments.parse(args);
   l.capture(
-    () => runZonedGuarded(
+    () => runZonedGuarded<void>(
       () async {
         l.i('Preparing database');
         final db = Database.lazy();
+        collectLogs(db, logsBuffer);
         await db.refresh();
         l.i('Starting bot');
-        Bot(token: arguments.token, offset: db.getKey<int>(updateIdKey), onUpdate: handler(db)).start();
+        final lastUpdateId = db.getKey<int>(updateIdKey);
+        Bot(token: arguments.token, offset: lastUpdateId, onUpdate: handler(db)).start();
       },
       (error, stackTrace) {
         l.e('An top level error occurred. $error', stackTrace);
@@ -38,13 +44,48 @@ void main(List<String> args) {
   );
 }
 
-void Function(int updateId, Map<String, Object?> update) handler(Database db) {
+/// Collects logs from the buffer and saves them to the database every 5 seconds.
+void collectLogs(Database db, Queue<LogMessage> buffer, {Duration interval = const Duration(seconds: 5)}) {
+  void saveLogs(Timer timer) =>
+      Future<void>(() async {
+        try {
+          if (buffer.isEmpty) return;
+          final rows = buffer
+              .map<LogTblCompanion>(
+                (e) => LogTblCompanion.insert(
+                  level: e.level.level,
+                  message: e.message.toString(),
+                  time: Value(e.timestamp.millisecondsSinceEpoch),
+                  stack: switch (e) {
+                    LogMessageError msg => Value<String?>(msg.stackTrace.toString()),
+                    _ => const Value<String?>.absent(),
+                  },
+                ),
+              )
+              .toList(growable: false);
+          buffer.clear();
+          await db.batch((batch) {
+            batch.insertAll(db.logTbl, rows);
+          });
+          //l.d('Inserted ${rows.length} logs');
+        } on Object catch (e, s) {
+          l.e('Failed to insert logs: $e', s);
+        }
+      }).ignore();
+  Timer.periodic(interval, saveLogs);
+}
+
+/// Returns a handler that processes updates and saves the offset to the database.
+void Function(int updateId, Map<String, Object?> update) handler(
+  Database db, {
+  Duration interval = const Duration(seconds: 5),
+}) {
   final messageHandler = MessageHandler();
 
   var lastOffset = 0;
 
   // Periodically update the offset
-  Timer.periodic(const Duration(seconds: 5), (_) async {
+  Timer.periodic(interval, (_) async {
     if (lastOffset <= (db.getKey<int>(updateIdKey) ?? 0)) return;
     db.setKey(updateIdKey, lastOffset);
     l.d('Updated offset to $lastOffset');
