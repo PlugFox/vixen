e60b2379270f5fd4aad61e2f46816662e208c00f
Mike Matiunin <plugfox@gmail.com>
2025-02-22 19:46:02 +0400

    Add captcha generator

--- lib/vixen.dart
+++ lib/vixen.dart
@@ -2,6 +2,7 @@ library;
 
 export 'src/arguments.dart';
 export 'src/bot.dart';
+export 'src/captcha.dart';
 export 'src/constant/constants.dart';
 export 'src/constant/pubspec.yaml.g.dart';
 export 'src/database.dart';
--- test/unit_test.dart
+++ test/unit_test.dart
@@ -5,4 +5,19 @@ void main() => group('unit', () {
   test('arguments', () {
     expect(() => Arguments.parse(['-t 123']), returnsNormally);
   });
+
+  test('create_captcha', () {
+    final generator = CaptchaGenerator();
+    expectLater(generator.generate(length: 6), completes);
+    expectLater(
+      generator.generate(length: 6),
+      completion(
+        isA<Captcha>()
+            .having((c) => c.length, 'length', 6)
+            .having((c) => c.numbers.length == c.length, 'numbers', isTrue)
+            .having((c) => c.text.length == c.length, 'text', isTrue)
+            .having((c) => c.image.length, 'image', greaterThan(0)),
+      ),
+    );
+  });
 });
